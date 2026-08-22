8b3d276d62712262a9840b3453a63f4d42a21be3
SPARTAN-175 <avengers_0077@hotmail.com>
2026-08-21 19:58:13 -0600

    Update dashboard-conductor.js

--- js/dashboard-conductor.js
+++ js/dashboard-conductor.js
@@ -296,6 +296,10 @@ onAuthStateChanged(
 
 
             actualizarVista();
+            
+            escucharEstadoRepartidor(
+            user
+            );
 
 
             // =============================
